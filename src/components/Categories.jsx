d363ce3ae7a7aa017812c3aadbcd1d4215a18795
UlfathAraIslam <mouliajsahid@gmail.com>
2025-05-03 07:26:02 +0900

    categories displayed dynamically

---
+++ src/components/Categories.jsx
@@ -0,0 +1,28 @@
+import React, { use } from "react";
+import { NavLink } from "react-router";
+const categoryPromise = fetch("/public/categories.json").then((res) =>
+  res.json()
+);
+
+const Categories = () => {
+  const categories = use(categoryPromise);
+
+  return (
+    <div>
+      <h2 className="font-bold">All Categories ({categories.length})</h2>
+      <div className="grid grid-cols-1 mt-5 gap-3">
+        {categories.map((category) => (
+          <NavLink
+            to={`/category/${category.id}`}
+            key={category.id}
+            className={"btn bg-white border-0 hover:bg-base-200 font-semibold"}
+          >
+            {category.name}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- src/components/homeLayout/LeftAside.jsx
+++ src/components/homeLayout/LeftAside.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import Categories from '../Categories';
 
 const LeftAside = () => {
     return (
         <div>
-            LeftAside
+            <Suspense fallback={<span className="loading loading-ball loading-xl"></span>}>
+            <Categories></Categories>
+            </Suspense>
         </div>
     );
 };
