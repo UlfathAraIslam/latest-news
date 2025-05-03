225d62e7c95999355c7ed347fff9695773f9bd99
UlfathAraIslam <mouliajsahid@gmail.com>
2025-05-03 10:53:33 +0900

    qzone is designed

--- src/components/homeLayout/RightAside.jsx
+++ src/components/homeLayout/RightAside.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SocialLogin from './SocialLogin';
 import FindUs from './FindUs';
+import QZone from './QZone';
 
 const RightAside = () => {
     return (
         <div className='space-y-5'>
             <SocialLogin></SocialLogin>
             <FindUs></FindUs>
+            <QZone></QZone>
         </div>
     );
 };
